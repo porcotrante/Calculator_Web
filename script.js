d5ea82cba548649d457670f30cda12b58d00d739
Pedro Wagner <pedrowagner0801@gmail.com>
2024-01-29 20:58:04 -0300

    added functional operation buttons

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const digitCont = document.querySelector('.digitCont');
+const display = document.querySelector('.display');
 
 let ID = 1;
 
@@ -6,27 +7,59 @@ for (let i = 0; i < 4; i++){
     const line = document.createElement('div');
     line.classList.add('line');
 
-    for (let j = 0; j < 3; j++){
+    for (let j = 0; j < 4; j++){
         const col = document.createElement('button');
         col.classList.add('col');
         col.setAttribute("id", ID.toString());
 
+        col.textContent = (ID).toString();
+
+        if (col.id == 4){
+            col.textContent = '+';
+            col.style.backgroundColor = 'darkblue'
+            col.style.color = 'white'
+        }
+
+        else if (col.id == 8){
+            col.textContent = '-';
+            col.style.backgroundColor = 'darkblue'
+            col.style.color = 'white'
+        }
+
+        else if (col.id == 12){
+            col.textContent = 'x';
+            col.style.backgroundColor = 'darkblue'
+            col.style.color = 'white'
+        }
+
+        else if (col.id == 16){
+            col.textContent = '/';
+            col.style.backgroundColor = 'darkblue'
+            col.style.color = 'white'
+        }
+
+        else if (col.id > 4 && col.id < 8){
+            col.textContent = (ID - 1).toString();
+        }
+
+        else if (col.id > 8 && col.id < 12){
+            col.textContent = (ID - 2).toString();
+        }
+
+        else if (col.id == 13){
+            col.textContent = '0';
+        }
+
+        else if (col.id == 14){
+            col.textContent = 'clear';
+        }
+
+        else if (col.id == 15){
+            col.textContent = '=';
+            col.style.backgroundColor = 'lightsalmon';
+        }
+
         ID++;
-
-        col.textContent = (ID-1).toString();
-
-        if (ID == 11) {
-            col.textContent = "0"
-        }
-
-        if (ID == 12) {
-            col.textContent = "Clear"
-        }
-
-        if (ID == 13) {
-            col.textContent = "="
-            col.style.backgroundColor = "lightsalmon";
-        }
 
         line.appendChild(col);
     }
@@ -36,18 +69,18 @@ for (let i = 0; i < 4; i++){
 
 const digits = document.querySelectorAll('.col');
 
-const display = document.querySelector('.display');
-
-for (let i = 0; i < 9; i++){
+for (let i = 0; i < 13; i++){
     digits[i].addEventListener('click', () => {
-        display.textContent = display.textContent + digits[i].id;
+        display.textContent = display.textContent + digits[i].textContent;
     })
 }
 
-digits[9].addEventListener('click', () => {
-    display.textContent = display.textContent + '0';
-})
-
-digits[10].addEventListener('click', () =>{
+digits[13].addEventListener('click', () =>{
     display.textContent = '';
 })
+
+digits[15].addEventListener('click', () => {
+    display.textContent = display.textContent + digits[15].textContent;
+})
+
+console.log(digits[15].textContent);
